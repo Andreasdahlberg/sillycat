619039ac3a32071e42f29bdcb8ec5a26ae165b5f
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2014-11-20 17:25:58 +0100

    Updated project structure.

--- firmware/sillycat_firmware/Debug/makedep.mk
+++ firmware/sillycat_firmware/Debug/makedep.mk
@@ -2,6 +2,8 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
+libADC.c
+
 libDebug.c
 
 libDHT22.c
--- firmware/sillycat_firmware/sillycat_firmware.c
+++ firmware/sillycat_firmware/sillycat_firmware.c
@@ -23,17 +23,26 @@ along with SillyCat firmware.  If not, see <http://www.gnu.org/licenses/>.
 #include "libLED.h"
 #include "libTimer.h"
 #include "libDHT22.h"
+#include "libADC.h"
+#include "libPower.h"
 
 #define LIBNAME "Main"
+#define DEBUG_STR(message) libDebug_PrintString(LIBNAME, message)
+#define DEBUG_INT(num)     libDebug_PrintInteger(LIBNAME, num)
 
 
 int main(void)
 {
-	libTimer_Init();
+
 	libDebug_Init();
+		DEBUG_STR("Init system");
+	
+	libADC_Init();
+	libPower_Init();  
+	libTimer_Init();
 	libLED_Init();
 	libDHT22_Init();		
-
+	
     while(1)
     {
 		
@@ -41,17 +50,9 @@ int main(void)
 		
 		//libDebug_PrintString(LIBNAME, "I love silly cats");
 		//libDebug_PrintUnsignedInteger(LIBNAME, libTimer_GetSeconds());
+		libADC_Update();
+		libPower_Update();
 		libLED_Update();
 		libDHT22_Update();
-/*		libLED_Toggle();
-		_delay_ms(10);
-		libLED_Toggle();
-		_delay_ms(150);
-		libLED_Toggle();
-		_delay_ms(10);
-		libLED_Toggle();*/
-		//old_time = system_timer;
-		//_delay_ms(5000);
-
     }
 }
