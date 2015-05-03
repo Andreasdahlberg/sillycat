29233cbf0ba81fd6351c9bf315a554315239e0de
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2015-05-03 12:29:17 +0200

    Changed size and orientation of ubuntu mono font.

--- firmware/main_firmware/fonts/font_ubuntu_mono.h
+++ firmware/main_firmware/fonts/font_ubuntu_mono.h
@@ -1,7 +1,7 @@
 #include "bitmap_db.h"
 
-// Font data for Ubuntu Mono 8pt
-extern const uint8_t ubuntuMono_8ptBitmaps[];
-extern const FONT_INFO ubuntuMono_8ptFontInfo;
-extern const FONT_CHAR_INFO ubuntuMono_8ptDescriptors[];
+/* Font data for Ubuntu Mono 10pt */
+extern const uint8_t ubuntuMono_10ptBitmaps[];
+extern const FONT_INFO ubuntuMono_10ptFontInfo;
+extern const FONT_CHAR_INFO ubuntuMono_10ptDescriptors[];
 
