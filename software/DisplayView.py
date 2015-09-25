e72f9b0eab67f4ca6f2f04872932b3141014a6ea
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2015-09-25 18:59:19 +0200

    Implemented method to save view to file. Fixed mixed usage of tabs and spaces. Refactored code for adding color to method add_color.

--- software/DisplayView.py
+++ software/DisplayView.py
@@ -29,17 +29,17 @@ VRAM_COLUMNS = 128
 VRAM_SIZE = (VRAM_PAGES * VRAM_COLUMNS)
 
 def create_bitmap_image(rows, w):
-	mult4 = lambda n: int(math.ceil(n/4))*4
-	mult8 = lambda n: int(math.ceil(n/8))*8
-	lh = lambda n: struct.pack("<h", n)
-	li = lambda n: struct.pack("<i", n)
+    mult4 = lambda n: int(math.ceil(n/4))*4
+    mult8 = lambda n: int(math.ceil(n/8))*8
+    lh = lambda n: struct.pack("<h", n)
+    li = lambda n: struct.pack("<i", n)
 
-	h, wB = len(rows), int(mult8(w)/8)
-	s, pad = li(mult4(wB)*h+0x20), [0]*(mult4(wB)-wB)
-	s = li(mult4(w)*h+0x20)
-	return (b"BM" + s + b"\x00\x00\x00\x00\x20\x00\x00\x00\x0C\x00\x00\x00" +
-		lh(w) + lh(h) + b"\x01\x00\x01\x00\xff\xff\xff\x00\x00\x00" +
-		b"".join([bytes(row+pad) for row in reversed(rows)]))
+    h, wB = len(rows), int(mult8(w)/8)
+    s, pad = li(mult4(wB)*h+0x20), [0]*(mult4(wB)-wB)
+    s = li(mult4(w)*h+0x20)
+    return (b"BM" + s + b"\x00\x00\x00\x00\x20\x00\x00\x00\x0C\x00\x00\x00" +
+        lh(w) + lh(h) + b"\x01\x00\x01\x00\xff\xff\xff\x00\x00\x00" +
+        b"".join([bytes(row+pad) for row in reversed(rows)]))
 
 
 class DisplayView(QtGui.QLabel):
@@ -49,27 +49,33 @@ class DisplayView(QtGui.QLabel):
         self.setPixmap(self._pixmap)
 
 
-    def update_display_view(self, image_data):
-        self._pixmap.loadFromData(self.format_image_data(bytes(image_data)))
-
-        display_pixmap = self._pixmap.scaled(384, 93)
-        mask = display_pixmap.createMaskFromColor(QtGui.QColor(0, 0, 0), QtCore.Qt.MaskOutColor)
-
-        p = QtGui.QPainter(display_pixmap)
+    def add_color(self, pixmap):
+        mask = pixmap.createMaskFromColor(QtGui.QColor(0, 0, 0), QtCore.Qt.MaskOutColor)
+        
+        p = QtGui.QPainter(pixmap)
         p.setPen(QtGui.QColor(255, 201, 14))
 
-        p.drawPixmap(display_pixmap.rect(), mask, mask.rect())
+        p.drawPixmap(pixmap.rect(), mask, mask.rect())
         p.end() 
 
-        mask = display_pixmap.createMaskFromColor(QtGui.QColor(255, 255, 255), QtCore.Qt.MaskOutColor)
+        mask = pixmap.createMaskFromColor(QtGui.QColor(255, 255, 255), QtCore.Qt.MaskOutColor)
 
-        p = QtGui.QPainter(display_pixmap)
+        p = QtGui.QPainter(pixmap)
         p.setPen(QtGui.QColor(0, 0, 0))
-        p.drawPixmap(display_pixmap.rect(), mask, mask.rect())
-        p.end()     
+        p.drawPixmap(pixmap.rect(), mask, mask.rect())
+        p.end()
+
+        return pixmap   
 
 
-        self.setPixmap(display_pixmap)
+    def save_view_to_file(self, filename="display.png"):
+        self._pixmap.save(filename, 'PNG')
+
+
+    def update_display_view(self, image_data):
+        self._pixmap.loadFromData(self.format_image_data(bytes(image_data)))
+        self.setPixmap(self.add_color(self._pixmap).scaled(384, 93))
+        self.save_view_to_file()
 
 
     def format_image_data(self, image_data):
