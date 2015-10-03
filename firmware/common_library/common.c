385f21f0703f8605eea5f0b0c6d397644994f684
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2015-10-03 12:05:50 +0200

    Improved document format.

--- firmware/common_library/common.c
+++ firmware/common_library/common.c
@@ -1,3 +1,12 @@
+/**
+ * @file   common.c
+ * @Author Andreas Dahlberg (andreas.dahlberg90@gmail.com)
+ * @date   2015-10-03 (Last edit)
+ * @brief  Implementation of common functions
+ *
+ * Detailed description of file.
+ */
+
 /*
 This file is part of SillyCat firmware.
 
@@ -15,55 +24,77 @@ You should have received a copy of the GNU General Public License
 along with SillyCat firmware.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+//////////////////////////////////////////////////////////////////////////
+//INCLUDES
+//////////////////////////////////////////////////////////////////////////
+
 #include <avr/wdt.h>
 
 #include "common.h"
 
+//////////////////////////////////////////////////////////////////////////
+//DEFINES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//TYPE DEFINITIONS
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//VARIABLES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//LOCAL FUNCTION PROTOTYPES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//FUNCTIONS
+//////////////////////////////////////////////////////////////////////////
 
 uint8_t BCDToDecimal(uint8_t bcd_data)
 {
-	uint8_t data;
-		
-	data = ((bcd_data & 0xF0)>>4)*10;
-	data += (bcd_data & 0x0F);
-	
-	return data;
+    uint8_t data;
+        
+    data = ((bcd_data & 0xF0)>>4)*10;
+    data += (bcd_data & 0x0F);
+    
+    return data;
 }
 
 uint16_t DecimalToBCD(uint8_t decimal_data)
 {
-	uint16_t data = 0;
-	uint8_t shift = 0;
-	
-	do
-	{
-		data |= (decimal_data % 10) << shift;
-		shift += 4;
-		decimal_data = decimal_data / 10;
-	}
-	while (decimal_data > 0);
-	
-	return data;
+    uint16_t data = 0;
+    uint8_t shift = 0;
+    
+    do
+    {
+        data |= (decimal_data % 10) << shift;
+        shift += 4;
+        decimal_data = decimal_data / 10;
+    }
+    while (decimal_data > 0);
+    
+    return data;
 }
 
 void SetBit(uint8_t bit_index, bool state, uint8_t *data)
 {
-	if (state == TRUE)
-	{
-		
-		*data |= (1 << bit_index);
-	}
-	else
-	{
-		*data &= ~(1 << bit_index);
-	}
+    if (state == TRUE)
+    {
+        *data |= (1 << bit_index);
+    }
+    else
+    {
+        *data &= ~(1 << bit_index);
+    }
 }
 
 int GetFreeRam()
 {
-	extern int __heap_start, *__brkval;
-	int v;
-	return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
+    extern int __heap_start, *__brkval;
+    int v;
+    return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
 }
 
 void SoftReset()  
@@ -73,3 +104,7 @@ void SoftReset()
     {                       
     }                       
 }
+
+//////////////////////////////////////////////////////////////////////////
+//LOCAL FUNCTIONS
+//////////////////////////////////////////////////////////////////////////
