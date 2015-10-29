18ce5db15d601726ba97f5e451f77e7386d460a3
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2015-10-29 21:40:10 +0100

    More work on Transceiver.

--- firmware/main_firmware/Transceiver.c
+++ firmware/main_firmware/Transceiver.c
@@ -30,12 +30,17 @@ along with SillyCat firmware.  If not, see <http://www.gnu.org/licenses/>.
 
 #define F_CPU 8000000UL // 8 MHz
 
+#include <util/delay.h>
+#include <string.h>
+
 #include "common.h"
 #include "libDebug.h"
 #include "libRFM69.h"
 #include "libTimer.h"
 
-#include <util/delay.h>
+#include "Transceiver.h"
+#include "RTC.h"
+#include "Sensor.h"
 
 //////////////////////////////////////////////////////////////////////////
 //DEFINES
@@ -45,28 +50,31 @@ along with SillyCat firmware.  If not, see <http://www.gnu.org/licenses/>.
 //TYPE DEFINITIONS
 //////////////////////////////////////////////////////////////////////////
 
-typedef struct 
-{
-    uint8_t length;
-    uint8_t address;
-    uint8_t data[66];
-}transceiver_packet_type;
-
 typedef enum
 {
     TR_STATE_NO_INIT = 0,
     TR_STATE_IDLE,
     TR_STATE_LISTENING,
     TR_STATE_SENDING,
-}transceiver_state_type;
+} transceiver_state_type;
 
 typedef enum
 {
     TR_STATE_SENDING_INIT = 0,
     TR_STATE_SENDING_WRITING,
     TR_STATE_SENDING_TRANSMITTING,
+    TR_STATE_SENDING_WAIT_FOR_ACK,
     TR_STATE_SENDING_DONE,
-}transceiver_sending_state_type;
+} transceiver_sending_state_type;
+
+typedef enum
+{
+    TR_STATE_LISTENING_INIT = 0,
+    TR_STATE_LISTENING_WAITING,
+    TR_STATE_LISTENING_RECEIVING,
+    TR_STATE_LISTENING_SEND_ACK,
+    TR_STATE_LISTENING_DONE,
+} transceiver_listening_state_type;
 
 //////////////////////////////////////////////////////////////////////////
 //VARIABLES
@@ -74,8 +82,10 @@ typedef enum
 
 //static char aes_key[17] = "1DUMMYKEYFOOBAR1";
 static uint8_t net_id[6] = {0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
-   
+
 static transceiver_state_type transceiver_state = TR_STATE_NO_INIT;
+
+static packet_frame_type packet_frame;
 
 //////////////////////////////////////////////////////////////////////////
 //LOCAL FUNCTION PROTOTYPES
@@ -83,6 +93,10 @@ static transceiver_state_type transceiver_state = TR_STATE_NO_INIT;
 
 transceiver_state_type IdleStateMachine(void);
 transceiver_state_type SendingStateMachine(void);
+transceiver_state_type ListeningStateMachine(void);
+
+bool ReadyToSend(void);
+bool PacketToSend(void);
 
 //////////////////////////////////////////////////////////////////////////
 //FUNCTIONS
@@ -92,116 +106,318 @@ void Transceiver_Init(void)
 {
     libRFM69_Init();
     //IMPORTANT: Only disable encryption during development!
-	libRFM69_EnableEncryption(FALSE);
-	libRFM69_EnableSequencer(TRUE);
-	libRFM69_EnableListenMode(FALSE);
-    
-	libRFM69_SetMode(RFM_STANDBY);
-	libRFM69_WaitForModeReady();
-    
-	libRFM69_SetBitRate(4800);
-	libRFM69_SetDataMode(RFM_PACKET_DATA);
-	libRFM69_SetModulationType(RFM_FSK);
-	libRFM69_SetModulationShaping(0x00);
-	libRFM69_SetFrequencyDeviation(5000);
-	libRFM69_SetCarrierFrequency(868000000);
-	libRFM69_EnableSyncWordGeneration(TRUE);
-	libRFM69_SetFIFOFillCondition(RFM_FIFO_FILL_AUTO);
-    
-    //TODO: Decide how network id is set, only set this if master?
-	libRFM69_SetSyncWordSize(5);
-	libRFM69_SetSyncWord(net_id, 6);
-    
-	libRFM69_SetTXStartCondition(RFM_TX_START_NOT_EMPTY);
-	libRFM69_SetNodeAddress(NODE_ADDRESS);
-	libRFM69_ClearFIFOOverrun();
-	libRFM69_SetPacketFormat(RFM_PACKET_VARIABLE_LEN);
-    
-    
-    uint32_t start = libTimer_GetMilliseconds();
-    libRFM69_SetMode(RFM_SLEEP);
+    libRFM69_EnableEncryption(FALSE);
+    libRFM69_EnableSequencer(TRUE);
+    libRFM69_EnableListenMode(FALSE);
+
+    libRFM69_SetMode(RFM_STANDBY);
     libRFM69_WaitForModeReady();
-    
-    DEBUG("Waited for: %u ms\r\n", libTimer_TimeDifference(start));
-	    
+
+    libRFM69_SetBitRate(4800);
+    libRFM69_SetDataMode(RFM_PACKET_DATA);
+    libRFM69_SetModulationType(RFM_FSK);
+    libRFM69_SetModulationShaping(0x00);
+    libRFM69_SetFrequencyDeviation(5000);
+    libRFM69_SetCarrierFrequency(868000000);
+    libRFM69_EnableSyncWordGeneration(TRUE);
+    libRFM69_SetFIFOFillCondition(RFM_FIFO_FILL_AUTO);
+
+    //TODO: Decide how network id is set, only set this if master?
+    libRFM69_SetSyncWordSize(5);
+    libRFM69_SetSyncWord(net_id, 6);
+
+    libRFM69_SetTXStartCondition(RFM_TX_START_NOT_EMPTY);
+    libRFM69_SetNodeAddress(NODE_ADDRESS);
+    libRFM69_ClearFIFOOverrun();
+    libRFM69_SetPacketFormat(RFM_PACKET_VARIABLE_LEN);
+
+    libRFM69_SetMode(RFM_RECEIVER);
+    libRFM69_WaitForModeReady();
+
+    memset(&packet_frame, 0, sizeof(packet_frame_type));
+#ifdef DEBUG_ENABLE
+    transceiver_state = TR_STATE_LISTENING;
+#else
     transceiver_state = TR_STATE_IDLE;
+#endif
+
+
     INFO("Transceiver initiated");
 }
 
 void Transceiver_Update(void)
 {
+
     switch (transceiver_state)
     {
         case TR_STATE_IDLE:
+            //DEBUG("IDLE\r\n");
             transceiver_state = IdleStateMachine();
             break;
 
         case TR_STATE_LISTENING:
+            //DEBUG("Listening\r\n");
+            transceiver_state = ListeningStateMachine();
             break;
-            
+
         case TR_STATE_SENDING:
+            //DEBUG("Sending\r\n");
             transceiver_state = SendingStateMachine();
             break;
-            
+
         default:
             break;
     }
     return;
 }
 
-bool Transceiver_Send(void)
+
+void DumpPacketHeader(packet_header_type *header)
 {
-   
-    
-    return FALSE;
+    DEBUG("******Packet header******\r\n");
+    DEBUG("Target: %u\r\n", header->target);
+    DEBUG("Source: %u\r\n", header->source);
+    DEBUG("Ack: %u\r\n", (uint8_t)header->ack);
+    DEBUG("RSSI: %d\r\n", header->rssi);
+    DEBUG("Total size: %u\r\n", header->total_size);
+}
+
+void DumpPacketContent(packet_content_type *content)
+{
+    uint8_t idx;
+    sensor_sample_type *reading;
+
+    DEBUG("******Packet content******\r\n");
+    DEBUG("Timestamp: 20%02u-%02u-%02u %02u:%02u:%02u\r\n",
+    content->timestamp.year,
+    content->timestamp.month,
+    content->timestamp.date,
+    content->timestamp.hour,
+    content->timestamp.minute,
+    content->timestamp.second);
+    DEBUG("Type: %u\r\n", (uint8_t)content->type);
+    DEBUG("Size: %u\r\n", (uint8_t)content->size);
+    DEBUG("Data: 0x");
+
+
+    for (idx = 0; idx < content->size; ++idx)
+    {
+        DEBUG("%02X", content->data[idx]);
+    }
+    DEBUG("\r\n");
+
+reading = (sensor_sample_type*)&content->data[1];
+
+DEBUG("Temperature: %u\r\n", reading->value/10);
+DEBUG("Temperature max: %u\r\n", reading->max/10);
+DEBUG("Temperature min: %u\r\n", reading->min/10);
+DEBUG("Temperature avg: %u\r\n", reading->average/10);
+
+}
+
+bool Transceiver_SendPacket(uint8_t target,
+                            bool request_ack,
+                            packet_content_type *content,
+                            transceiver_callback_type callback)
+{
+    bool status = TRUE;
+    packet_frame_type *frame_ptr;
+
+    frame_ptr = &packet_frame;
+
+    if (content->size > CONTENT_DATA_SIZE)
+    {
+        ERROR("Size of packet content is to large");
+        status = FALSE;
+    }
+
+    DEBUG("Ready to send: %u\r\n", (uint8_t)ReadyToSend());
+
+    if (ReadyToSend() && status)
+    {
+
+        frame_ptr->header.target = target;
+        frame_ptr->header.source = NODE_ADDRESS;
+        frame_ptr->header.ack = request_ack;
+        frame_ptr->header.rssi = libRFM69_GetRSSI();
+        frame_ptr->header.total_size = sizeof(packet_header_type) + 8 + content->size; //TODO: sizeof or define!
+
+        frame_ptr->content.timestamp = content->timestamp;
+        frame_ptr->content.type = content->type;
+        frame_ptr->content.size = content->size;
+        memcpy(frame_ptr->content.data, content->data, content->size);
+
+        frame_ptr->callback = callback;
+
+
+    }
+
+    return status;
 }
 
 //////////////////////////////////////////////////////////////////////////
 //LOCAL FUNCTIONS
 //////////////////////////////////////////////////////////////////////////
 
+//TODO: Make this function public?
+bool ReadyToSend(void)
+{
+    return (transceiver_state == TR_STATE_IDLE && libRFM69_GetRSSI() < -90);
+}
+
+bool PacketToSend(void)
+{
+    return (packet_frame.header.target > 0);
+}
+
+bool HandlePayload(void)
+{
+    uint8_t length;
+    uint8_t data_buffer[RFM_FIFO_SIZE - 1];
+
+
+    libRFM69_ReadFromFIFO(data_buffer, 1);
+    length = data_buffer[0];
+
+
+    if (length > RFM_FIFO_SIZE - 1)
+    {
+        ERROR("Size of data packet is larger then the FIFO");
+    }
+
+    libRFM69_ReadFromFIFO(&data_buffer[1], length);
+
+
+
+    DumpPacketHeader((packet_header_type*)data_buffer);
+    DumpPacketContent((packet_content_type*)&data_buffer[5]);
+
+    //DumpPacket(()data_buffer)
+
+    return TRUE;
+}
+
 transceiver_state_type IdleStateMachine(void)
 {
-   static uint32_t start = 0;
+    transceiver_state_type next_state = TR_STATE_IDLE;
 
-   if (libTimer_TimeDifference(start) > 500)
-   {
-       start = libTimer_GetMilliseconds();
-       //DEBUG("Idle...\r\n");
-   }       
-   
+    if (PacketToSend())
+    {
+        INFO("Sending packet from buffer");
+        next_state = TR_STATE_SENDING;
+    }
 
-   return TR_STATE_IDLE;
+    return next_state;
+}
+
+transceiver_state_type ListeningStateMachine(void)
+{
+    static transceiver_listening_state_type state = TR_STATE_LISTENING_INIT;
+
+    switch (state)
+    {
+        case TR_STATE_LISTENING_INIT:
+            libRFM69_SetMode(RFM_RECEIVER);
+            state = TR_STATE_LISTENING_WAITING;
+            break;
+
+        case TR_STATE_LISTENING_WAITING:
+            if (libRFM69_IsPayloadReady())
+            {
+                if (HandlePayload())
+                {
+                    // state = TR_STATE_LISTENING_DONE;
+                    // libRFM69_SetMode(RFM_STANDBY);
+                }
+            }
+            break;
+
+        case TR_STATE_LISTENING_SEND_ACK:
+            if (libRFM69_IsModeReady())
+            {
+                //Transceiver_SendPacket() //Send ACK-packet, async?
+            }
+            break;
+
+        default:
+            CRITICAL("Invalid state");
+            break;
+    }
+    return TR_STATE_LISTENING;
 }
 
 transceiver_state_type SendingStateMachine(void)
 {
     static transceiver_sending_state_type state = TR_STATE_SENDING_INIT;
-    
+    transceiver_state_type next_state = TR_STATE_SENDING;
+
     switch (state)
     {
         case TR_STATE_SENDING_INIT:
             libRFM69_SetMode(RFM_STANDBY);
+            state = TR_STATE_SENDING_WRITING;
+            break;
+
+        case TR_STATE_SENDING_WRITING:
             if (libRFM69_IsModeReady())
             {
-                state = TR_STATE_SENDING_WRITING;
-            }                
+                DEBUG("Write packet to FIFO\r\n");
+                libRFM69_WriteToFIFO((uint8_t*)&packet_frame.header, sizeof(packet_header_type));
+                libRFM69_WriteToFIFO((uint8_t*)&packet_frame.content, packet_frame.content.size + 8);
+                libRFM69_SetMode(RFM_TRANSMITTER);
+                state = TR_STATE_SENDING_TRANSMITTING;
+            }
             break;
-            
-        case TR_STATE_SENDING_WRITING:
-            break;
-            
+
         case TR_STATE_SENDING_TRANSMITTING:
+            if (libRFM69_IsPacketSent())
+            {
+                DEBUG("Packet sent\r\n");
+                if (packet_frame.header.ack)
+                {
+                    DEBUG("Waiting for ACK\r\n");
+                    libRFM69_SetMode(RFM_RECEIVER);
+                    state = TR_STATE_SENDING_WAIT_FOR_ACK;
+                }
+                else
+                {
+                    DEBUG("No ACK needed\r\n");
+                    libRFM69_SetMode(RFM_STANDBY);
+                    if (packet_frame.callback != NULL)
+                    {
+                        packet_frame.callback(TRUE);
+                    }
+                    memset(&packet_frame, 0, sizeof(packet_frame_type));
+                    state = TR_STATE_SENDING_INIT;
+                    next_state = TR_STATE_IDLE;
+                }
+            }
             break;
-            
+
+        case TR_STATE_SENDING_WAIT_FOR_ACK:
+            if (libRFM69_IsPayloadReady())
+            {
+                if (HandlePayload())
+                {
+
+                }
+
+                libRFM69_SetMode(RFM_STANDBY);
+                memset(&packet_frame, 0, sizeof(packet_frame_type));
+                state = TR_STATE_SENDING_INIT;
+                next_state = TR_STATE_IDLE;
+
+            }
+            break;
+
         case TR_STATE_SENDING_DONE:
             break;
-            
+
         default:
             ERROR("Unknown state, reset transceiver");
             state = TR_STATE_SENDING_INIT;
             Transceiver_Init();
             break;
     }
-    return TR_STATE_SENDING;
+    return next_state;
 }
--- firmware/main_firmware/Transceiver.h
+++ firmware/main_firmware/Transceiver.h
@@ -1,7 +1,7 @@
 /**
  * @file   Transceiver.h
  * @Author Andreas Dahlberg (andreas.dahlberg90@gmail.com)
- * @date   2015-09-20 (Last edit)
+ * @date   2015-10-18 (Last edit)
  * @brief  Header for Transceiver interface.
  *
  * Detailed description of file.
@@ -28,8 +28,68 @@ along with SillyCat firmware.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef TRANSCEIVER_H_
 #define TRANSCEIVER_H_
 
+//////////////////////////////////////////////////////////////////////////
+//INCLUDES
+//////////////////////////////////////////////////////////////////////////
+
+#include "libRFM69.h"
+#include "RTC.h"
+
+//////////////////////////////////////////////////////////////////////////
+//DEFINES
+//////////////////////////////////////////////////////////////////////////
+
+#define CONTENT_DATA_SIZE 20
+
+//////////////////////////////////////////////////////////////////////////
+//TYPE DEFINITIONS
+//////////////////////////////////////////////////////////////////////////
+
+typedef void (*transceiver_callback_type)(bool status);
+
+typedef enum
+{
+    TR_PACKET_TYPE_ACK = 1,
+    TR_PACKET_TYPE_NACK,
+    TR_PACKET_TYPE_READING,
+    TR_PACKET_TYPE_TIME
+}packet_type_type;
+
+
+typedef struct
+{
+    uint8_t total_size;
+    uint8_t target;
+    uint8_t source;
+    bool ack;
+    int8_t rssi;
+}packet_header_type;
+
+typedef struct
+{
+    rtc_time_type timestamp;
+    packet_type_type type;
+    uint8_t size;
+    uint8_t data[CONTENT_DATA_SIZE];
+}packet_content_type;
+
+typedef struct
+{
+    packet_header_type header;
+    packet_content_type content;
+    transceiver_callback_type callback;
+}packet_frame_type;
+
+
+//////////////////////////////////////////////////////////////////////////
+//FUNCTION PROTOTYPES
+//////////////////////////////////////////////////////////////////////////
+
 void Transceiver_Init(void);
 void Transceiver_Update(void);
 bool Transceiver_Send(void);
+bool Transceiver_SendPacket(uint8_t target, bool request_ack,
+                            packet_content_type *content,
+                            transceiver_callback_type callback);
 
 #endif /* TRANSCEIVER_H_ */
