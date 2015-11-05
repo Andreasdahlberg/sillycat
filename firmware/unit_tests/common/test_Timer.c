239e33f0d0a6a405ab7b0018f3259355612e69fd
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2015-11-05 23:17:27 +0100

    Added tests for Timer.

---
+++ firmware/unit_tests/common/test_Timer.c
@@ -0,0 +1,101 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+#include <stdio.h>
+#include <stdint.h>
+
+#include "test_Timer.h"
+#include "Timer.h"
+
+void __wrap_libTimer_Init(void)
+{
+	return;
+}
+
+uint32_t __wrap_libTimer_GetMilliseconds(void)
+{
+	return mock_type(uint32_t);
+}
+
+static void test_TimeDifference_zero(void **state)
+{
+    will_return(__wrap_libTimer_GetMilliseconds, 0);   
+    will_return(__wrap_libTimer_GetMilliseconds, 500); 
+
+    assert_int_equal(Timer_TimeDifference(0), 0);
+    assert_int_equal(Timer_TimeDifference(500), 0);     
+    
+    return;
+}
+
+static void test_TimeDifference_nonzero(void **state)
+{
+    will_return(__wrap_libTimer_GetMilliseconds, 100);   
+    will_return(__wrap_libTimer_GetMilliseconds, 500);  
+
+    assert_int_equal(Timer_TimeDifference(0), 100);
+    assert_int_equal(Timer_TimeDifference(100), 400);     
+    
+    return;    
+}
+
+static void test_TimeDifference_wraparound(void **state)
+{
+    uint32_t timestamp = (0xFFFFFFFF - 100);
+
+    will_return(__wrap_libTimer_GetMilliseconds, 0xFFFFFFFF);   
+    will_return(__wrap_libTimer_GetMilliseconds, 100);  
+
+    assert_int_equal(Timer_TimeDifference(timestamp), 100);   
+    assert_int_equal(Timer_TimeDifference(timestamp), 200);
+
+    return;
+}
+
+static void test_GetSeconds_exact(void **state)
+{
+    will_return(__wrap_libTimer_GetMilliseconds, 0);
+    will_return(__wrap_libTimer_GetMilliseconds, 2000);
+
+    assert_int_equal(Timer_GetSeconds(), 0);   
+    assert_int_equal(Timer_GetSeconds(), 2);    
+    return;
+}
+
+static void test_GetSeconds_ceil(void **state)
+{
+    will_return(__wrap_libTimer_GetMilliseconds, 0);
+    will_return(__wrap_libTimer_GetMilliseconds, 500);  
+    will_return(__wrap_libTimer_GetMilliseconds, 501);                  
+
+    assert_int_equal(Timer_GetSeconds(), 0);
+    assert_int_equal(Timer_GetSeconds(), 1);
+    assert_int_equal(Timer_GetSeconds(), 1);        
+    return;
+}
+
+static void test_GetSeconds_floor(void **state)
+{
+    will_return(__wrap_libTimer_GetMilliseconds, 0);
+    will_return(__wrap_libTimer_GetMilliseconds, 499);
+    will_return(__wrap_libTimer_GetMilliseconds, 1200); 
+
+    assert_int_equal(Timer_GetSeconds(), 0);
+    assert_int_equal(Timer_GetSeconds(), 0);
+    assert_int_equal(Timer_GetSeconds(), 1);        
+    return;
+}
+
+int main(void) {
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(test_TimeDifference_zero),
+        cmocka_unit_test(test_TimeDifference_nonzero), 
+        cmocka_unit_test(test_TimeDifference_wraparound),
+        cmocka_unit_test(test_GetSeconds_exact), 
+        cmocka_unit_test(test_GetSeconds_ceil),  
+        cmocka_unit_test(test_GetSeconds_floor), 
+    };
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
---
+++ firmware/unit_tests/common/test_Timer.h
@@ -0,0 +1,3 @@
+
+void __wrap_libTimer_Init(void);
+uint32_t __wrap_libTimer_GetMilliseconds(void);
