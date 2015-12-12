6725b42ef092fa40b2d4c45e3a62b463243ec579
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2015-12-12 11:48:11 +0100

    Added event typedefs.

---
+++ firmware/common_library/Event.h
@@ -0,0 +1,58 @@
+/**
+ * @file   Event.h
+ * @Author Andreas Dahlberg (andreas.dahlberg90@gmail.com)
+ * @date   2015-12-12 (Last edit)
+ * @brief  Header of Event
+ *
+ * Detailed description of file.
+ */
+
+/*
+This file is part of SillyCat firmware.
+
+SillyCat firmware is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SillyCat firmware is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SillyCat firmware.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef EVENT_H_
+#define EVENT_H_
+
+//////////////////////////////////////////////////////////////////////////
+//INCLUDES
+//////////////////////////////////////////////////////////////////////////
+
+#include "Timer.h"
+
+//////////////////////////////////////////////////////////////////////////
+//DEFINES
+//////////////////////////////////////////////////////////////////////////
+
+#define NewEvent(typeid) ((event_type){Timer_GetMilliseconds(), typeid})
+
+//////////////////////////////////////////////////////////////////////////
+//TYPE DEFINITIONS
+//////////////////////////////////////////////////////////////////////////
+
+typedef struct
+{
+    uint32_t timestamp;
+    uint32_t id;
+} event_type;
+
+typedef void (*event_listener)(event_type *event);
+
+//////////////////////////////////////////////////////////////////////////
+//FUNCTION PROTOTYPES
+//////////////////////////////////////////////////////////////////////////
+
+#endif /* EVENT_H_ */
