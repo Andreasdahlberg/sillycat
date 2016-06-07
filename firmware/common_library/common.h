6f07dc87b43254bd93a3af53450eae8ae842edb1
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2016-06-07 21:43:30 +0200

    Added define for supply voltage.

--- firmware/common_library/common.h
+++ firmware/common_library/common.h
@@ -42,6 +42,7 @@ along with SillyCat firmware.  If not, see <http://www.gnu.org/licenses/>.
 //////////////////////////////////////////////////////////////////////////
 
 #define F_CPU 8000000UL // 8 MHz
+#define SUPPLY_VOLTAGE_mV 3300
 
 //Valid if sample_window >> sample_freq
 #define CALCULATE_ALPHA(sample_freq, sample_window) ((uint32_t)131070 / ((uint32_t)sample_window / (uint32_t)sample_freq + 1))
