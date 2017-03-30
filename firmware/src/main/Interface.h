9aee90a9a3ca358206774149307ed806e5c94c04
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2017-03-30 19:20:53 +0200

    Add missing function prototype

--- firmware/src/main/Interface.h
+++ firmware/src/main/Interface.h
@@ -1,7 +1,7 @@
 /**
  * @file   Interface.h
  * @Author Andreas Dahlberg (andreas.dahlberg90@gmail.com)
- * @date   2015-11-11 (Last edit)
+ * @date   2017-03-30 (Last edit)
  * @brief  Header of Interface
  *
  * Detailed description of file.
@@ -58,6 +58,7 @@ void Interface_Update(void);
 void Interface_Refresh(void);
 void Interface_AddView(struct view *new_view);
 void Interface_AddChild(struct view *parent_view, struct view *child_view);
+void Interface_AddSibling(struct view *sibling_view, struct view *new_view);
 void Interface_RemoveView(struct view *view);
 void Interface_NextView(void);
 void Interface_PreviousView(void);
