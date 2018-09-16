ac47ffb8d5574923f700b1f98d7b6d18b1cdb493
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2018-09-16 21:40:07 +0200

    Remove deprecated mocks

--- firmware/tests/mocks/mock_libInput.h
+++
@@ -1,6 +0,0 @@
-
-#include "libInput.h"
-
-void __wrap_libInput_SetCallbacks(libinput_callback_type right_event,
-                                  libinput_callback_type left_event,
-                                  libinput_callback_type push_event);
