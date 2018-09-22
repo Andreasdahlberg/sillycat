d7632e0224354b8755b583a7fc1cc9185a21b80a
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2018-09-22 13:33:44 +0200

    Update libTimer mock

--- firmware/tests/mocks/mock_libTimer.h
+++ firmware/tests/mocks/mock_libTimer.h
@@ -1,3 +1,48 @@
+/**
+ * @file   mock_libTimer.h
+ * @Author Andreas Dahlberg (andreas.dahlberg90@gmail.com)
+ * @date   2018-09-22 (Last edit)
+ * @brief  Mock functions for libTimer module.
+ */
+
+/*
+This file is part of SillyCat firmware.
+
+SillyCat firmware is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SillyCat firmware is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SillyCat firmware.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef WRAP_LIBTIMER_H_
+#define WRAP_LIBTIMER_H_
+
+//////////////////////////////////////////////////////////////////////////
+//INCLUDES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//DEFINES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//TYPE DEFINITIONS
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//FUNCTION PROTOTYPES
+//////////////////////////////////////////////////////////////////////////
 
 void __wrap_libTimer_Init(void);
+void __wrap_libTimer_Reset(void);
 uint32_t __wrap_libTimer_GetMilliseconds(void);
+
+#endif
