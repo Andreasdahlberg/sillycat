2d3fe59c4584868b8e9725eed6021c56c6ab930b
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2018-09-22 20:27:22 +0200

    Update cmocka

--- scripts/install_cmocka.sh
+++ scripts/install_cmocka.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-wget https://cmocka.org/files/1.1/cmocka-1.1.0.tar.xz --no-check-certificate
-tar xf cmocka-1.1.0.tar.xz
+wget https://cmocka.org/files/1.1/cmocka-1.1.2.tar.xz --no-check-certificate
+tar xf cmocka-1.1.2.tar.xz
 
-mkdir cmocka-1.1.0/build
-cd cmocka-1.1.0/build
+mkdir cmocka-1.1.2/build
+cd cmocka-1.1.2/build
 
 cmake -DCMAKE_INSTALL_PREFIX=/usr ..
 make
