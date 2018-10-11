c964a303f7ec1ccf0cad0e2a3cce279527406779
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2018-10-11 22:31:27 +0200

    Implement unit tests for the MCU temperature driver

---
+++ firmware/tests/main/drivers/MCUTemperature/test_driverMCUTemperature.h
@@ -0,0 +1,44 @@
+/**
+ * @file   test_driverMCUTemperature.h
+ * @Author Andreas Dahlberg (andreas.dahlberg90@gmail.com)
+ * @date   2018-10-11 (Last edit)
+ * @brief  Test suite for the MCU temperature driver.
+ */
+
+/*
+This file is part of SillyCat firmware.
+
+SillyCat firmware is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SillyCat firmware is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SillyCat firmware.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef TEST_DRIVERMCUTEMPERATURE_H_
+#define TEST_DRIVERMCUTEMPERATURE_H_
+
+//////////////////////////////////////////////////////////////////////////
+//INCLUDES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//DEFINES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//TYPE DEFINITIONS
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//FUNCTION PROTOTYPES
+//////////////////////////////////////////////////////////////////////////
+
+#endif
