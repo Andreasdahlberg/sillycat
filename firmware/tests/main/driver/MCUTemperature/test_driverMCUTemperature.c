e1f4e33a19cbc06158a5a89798401dca8248056c
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2018-10-24 21:58:41 +0200

    Add more unit tests for the MCU temperature driver

--- firmware/tests/main/driver/MCUTemperature/test_driverMCUTemperature.c
+++ firmware/tests/main/driver/MCUTemperature/test_driverMCUTemperature.c
@@ -1,7 +1,7 @@
 /**
  * @file   test_driverMCUTemperature.c
  * @Author Andreas Dahlberg (andreas.dahlberg90@gmail.com)
- * @date   2018-10-11 (Last edit)
+ * @date   2018-10-24 (Last edit)
  * @brief  Test suite for the MCU temperature driver.
  */
 
@@ -61,6 +61,14 @@ along with SillyCat firmware.  If not, see <http://www.gnu.org/licenses/>.
 //LOCAL FUNCTIONS
 //////////////////////////////////////////////////////////////////////////
 
+static int Setup(void **state)
+{
+    expect_any(__wrap_ADC_InitChannel, index);
+    driverMCUTemperature_Init();
+
+    return 0;
+}
+
 //////////////////////////////////////////////////////////////////////////
 //TESTS
 //////////////////////////////////////////////////////////////////////////
@@ -78,6 +86,35 @@ static void test_driverMCUTemperature_GetSensor(void **state)
     assert_non_null(driverMCUTemperature_GetSensor());
 }
 
+static void test_driverMCUTemperature_UpdateSampleFrequency(void **state)
+{
+    for (size_t i = 0; i < 2; ++i)
+    {
+        struct sensor_t *sensor_p = driverMCUTemperature_GetSensor();
+
+        will_return(__wrap_Timer_TimeDifference, 100);
+        sensor_p->Update(sensor_p);
+
+        will_return(__wrap_Timer_TimeDifference, 101);
+        will_return(__wrap_ADC_Convert, 0);
+        will_return(__wrap_Timer_GetMilliseconds, 200);
+        sensor_p->Update(sensor_p);
+    }
+}
+
+static void test_driverMCUTemperature_Update(void **state)
+{
+    struct sensor_t *sensor_p = driverMCUTemperature_GetSensor();
+
+    will_return(__wrap_Timer_TimeDifference, UINT32_MAX);
+    will_return(__wrap_ADC_Convert, 100);
+    will_return(__wrap_Timer_GetMilliseconds, 200);
+    sensor_p->Update(sensor_p);
+
+    assert_int_equal(sensor_p->value, 33);
+    assert_true(sensor_p->valid);
+}
+
 //////////////////////////////////////////////////////////////////////////
 //FUNCTIONS
 //////////////////////////////////////////////////////////////////////////
@@ -87,7 +124,9 @@ int main(int argc, char *argv[])
     const struct CMUnitTest tests[] =
     {
         cmocka_unit_test(test_driverMCUTemperature_Init),
-        cmocka_unit_test(test_driverMCUTemperature_GetSensor)
+        cmocka_unit_test_setup(test_driverMCUTemperature_GetSensor, Setup),
+        cmocka_unit_test_setup(test_driverMCUTemperature_UpdateSampleFrequency, Setup),
+        cmocka_unit_test_setup(test_driverMCUTemperature_Update, Setup),
     };
 
     if (argc >= 2)
