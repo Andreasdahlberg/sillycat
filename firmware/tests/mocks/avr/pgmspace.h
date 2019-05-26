002fe14358aae4e4829e8ae6679b445f0da87199
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2019-05-26 20:30:28 +0200

    Proper solution for missing AVR header in unit test

--- firmware/tests/mocks/avr/pgmspace.h
+++ firmware/tests/mocks/avr/pgmspace.h
@@ -1,10 +1,8 @@
 /**
- * @file   libUI.h
+ * @file   pgmspace.h
  * @Author Andreas Dahlberg (andreas.dahlberg90@gmail.com)
  * @date   2019-05-26 (Last edit)
- * @brief  Header of UI-library.
- *
- * Detailed description of file.
+ * @brief  Fake pgmspace header.
  */
 
 /*
@@ -24,25 +22,16 @@ You should have received a copy of the GNU General Public License
 along with SillyCat firmware.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef LIBUI_H_
-#define LIBUI_H_
+#ifndef FAKEPGMSPACE_H_
+#define FAKEPGMSPACE_H_
 
 //////////////////////////////////////////////////////////////////////////
 //INCLUDES
 //////////////////////////////////////////////////////////////////////////
 
-#include <avr/pgmspace.h>
-
 //////////////////////////////////////////////////////////////////////////
 //DEFINES
 //////////////////////////////////////////////////////////////////////////
-
-#define libUI_Print(text, x_pos, y_pos, ...) libUI_Print_P(PSTR(text), x_pos, y_pos, ##__VA_ARGS__)
-
-// Standard y-positions for text baselines.
-#define UI_SINGLE_ROW 20
-#define UI_DOUBLE_ROW_FIRST 13
-#define UI_DOUBLE_ROW_SECOND 27
 
 //////////////////////////////////////////////////////////////////////////
 //TYPE DEFINITIONS
@@ -52,11 +41,4 @@ along with SillyCat firmware.  If not, see <http://www.gnu.org/licenses/>.
 //FUNCTION PROTOTYPES
 //////////////////////////////////////////////////////////////////////////
 
-void libUI_Update(void);
-void libUI_PrintText(const char *buffer, uint8_t x_pos, uint8_t y_pos);
-void libUI_Print_P(const char *text, uint8_t x_pos, uint8_t y_pos, ...);
-void libUI_DrawLine(uint8_t x_start, uint8_t y_start, uint8_t x_end, uint8_t y_end);
-void libUI_DrawRectangle(uint8_t x_start, uint8_t y_start, uint8_t width, uint8_t height);
-void libUI_DrawCircle(uint8_t x_pos, uint8_t y_pos, uint8_t radius);
-
-#endif /* LIBUI_H_ */
+#endif
