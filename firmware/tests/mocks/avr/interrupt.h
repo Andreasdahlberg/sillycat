c31100704fb424a0bfa94a41b9a3e30540e1dfb6
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2019-06-13 23:37:19 +0200

    Add fake avr/interrupt header

---
+++ firmware/tests/mocks/avr/interrupt.h
@@ -0,0 +1,47 @@
+/**
+ * @file   interrupt.h
+ * @Author Andreas Dahlberg (andreas.dahlberg90@gmail.com)
+ * @date   2019-06-13 (Last edit)
+ * @brief  Fake interrupt header.
+ */
+
+/*
+This file is part of SillyCat firmware.
+
+SillyCat firmware is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SillyCat firmware is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SillyCat firmware.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef FAKEINTERRUPT_H_
+#define FAKEINTERRUPT_H_
+
+//////////////////////////////////////////////////////////////////////////
+//INCLUDES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//DEFINES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//TYPE DEFINITIONS
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//FUNCTION PROTOTYPES
+//////////////////////////////////////////////////////////////////////////
+
+void cli(void);
+void sei(void);
+
+#endif
