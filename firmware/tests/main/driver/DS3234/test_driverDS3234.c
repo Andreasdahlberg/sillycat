2d1e727a656ab2be4f5d2144207b2fce1367eacb
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2021-01-10 23:42:13 +0100

    Prepare driverDS3234 tests

---
+++ firmware/tests/main/driver/DS3234/test_driverDS3234.c
@@ -0,0 +1,181 @@
+/**
+ * @file   test_driverDS3234.c
+ * @Author Andreas Dahlberg (andreas.dahlberg90@gmail.com)
+ * @date   2021-01-10 (Last edit)
+ * @brief  Test suite for the DS3234 RTC driver.
+ */
+
+/*
+This file is part of SillyCat firmware.
+
+SillyCat firmware is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SillyCat firmware is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SillyCat firmware.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//////////////////////////////////////////////////////////////////////////
+//INCLUDES
+//////////////////////////////////////////////////////////////////////////
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "driverDS3234.h"
+#include "driverDS3234Registers.h"
+
+//////////////////////////////////////////////////////////////////////////
+//DEFINES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//TYPE DEFINITIONS
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//VARIABLES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//LOCAL FUNCTION PROTOTYPES
+//////////////////////////////////////////////////////////////////////////
+
+static int Setup(void **state);;
+static void StubSPIPreCallback(void);
+static void StubSPIPostCallback(void);
+static void ExpectWriteRegister(uint8_t address);
+static void ExpectWriteValueRegister(uint8_t address, uint8_t data);
+static void ExpectReadRegister(uint8_t address, uint8_t data);
+static void ExpectModifyRegister(uint8_t address);
+static void ExpectResetControlRegister(void);
+
+//////////////////////////////////////////////////////////////////////////
+//INTERUPT SERVICE ROUTINES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//LOCAL FUNCTIONS
+//////////////////////////////////////////////////////////////////////////
+
+static int Setup(void **state)
+{
+    ExpectResetControlRegister();
+    driverDS3234_Init(StubSPIPreCallback, StubSPIPostCallback);
+
+    return 0;
+}
+
+static void StubSPIPreCallback(void)
+{
+
+}
+
+static void StubSPIPostCallback(void)
+{
+
+}
+
+static void ExpectWriteRegister(uint8_t address)
+{
+    expect_value(__wrap_libSPI_WriteByte, data, address | WRITE_ADDRESS);
+    expect_any(__wrap_libSPI_WriteByte, data);
+}
+
+static void ExpectWriteValueRegister(uint8_t address, uint8_t data)
+{
+    expect_value(__wrap_libSPI_WriteByte, data, address | WRITE_ADDRESS);
+    expect_value(__wrap_libSPI_WriteByte, data, data);
+}
+
+static void ExpectReadRegister(uint8_t address, uint8_t data)
+{
+    expect_value(__wrap_libSPI_WriteByte, data, address | READ_ADDRESS);
+    will_return(__wrap_libSPI_ReadByte, data);
+}
+
+static void ExpectModifyRegister(uint8_t address)
+{
+    ExpectReadRegister(address, 0);
+    ExpectWriteRegister(address);
+}
+
+static void ExpectResetControlRegister(void)
+{
+    ExpectWriteValueRegister(REG_CONTROL, 0x1C);
+}
+
+//////////////////////////////////////////////////////////////////////////
+//TESTS
+//////////////////////////////////////////////////////////////////////////
+
+static void test_driverDS3234_Init_InvalidCallbacks(void **state)
+{
+    expect_assert_failure(driverDS3234_Init(NULL, NULL));
+    expect_assert_failure(driverDS3234_Init(StubSPIPreCallback, NULL));
+    expect_assert_failure(driverDS3234_Init(NULL, StubSPIPostCallback));
+}
+
+static void test_driverDS3234_Init(void **state)
+{
+    ExpectResetControlRegister();
+    driverDS3234_Init(StubSPIPreCallback, StubSPIPostCallback);
+}
+
+//////////////////////////////////////////////////////////////////////////
+//FUNCTIONS
+//////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char *argv[])
+{
+    const struct CMUnitTest tests[] =
+    {
+        cmocka_unit_test(test_driverDS3234_Init_InvalidCallbacks),
+        cmocka_unit_test(test_driverDS3234_Init),
+    };
+
+    if (argc >= 2)
+    {
+        cmocka_set_test_filter(argv[1]);
+    }
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
+
+uint8_t __wrap_BCDToDecimal(uint8_t bcd_data)
+{
+    uint8_t data;
+
+    data = ((bcd_data & 0xF0) >> 4) * 10;
+    data += (bcd_data & 0x0F);
+
+    return data;
+}
+
+uint16_t __wrap_DecimalToBCD(uint8_t decimal_data)
+{
+    uint16_t data = 0;
+    uint8_t shift = 0;
+
+    do
+    {
+        data |= (decimal_data % 10) << shift;
+        shift += 4;
+        decimal_data = decimal_data / 10;
+    }
+    while (decimal_data > 0);
+
+    return data;
+}
---
+++ firmware/tests/main/driver/DS3234/test_driverDS3234.h
@@ -0,0 +1,49 @@
+/**
+ * @file   test_driverDS3234.h
+ * @Author Andreas Dahlberg (andreas.dahlberg90@gmail.com)
+ * @date   2021-01-10 (Last edit)
+ * @brief  Test suite for the DS3234 RTC driver.
+ */
+
+/*
+This file is part of SillyCat firmware.
+
+SillyCat firmware is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SillyCat firmware is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SillyCat firmware.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef TEST_DRIVERDS3234_H_
+#define TEST_DRIVERDS3234_H_
+
+//////////////////////////////////////////////////////////////////////////
+//INCLUDES
+//////////////////////////////////////////////////////////////////////////
+
+#include <stdint.h>
+
+//////////////////////////////////////////////////////////////////////////
+//DEFINES
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//TYPE DEFINITIONS
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+//FUNCTION PROTOTYPES
+//////////////////////////////////////////////////////////////////////////
+
+uint8_t __wrap_BCDToDecimal(uint8_t bcd_data);
+uint16_t __wrap_DecimalToBCD(uint8_t decimal_data);
+
+#endif
